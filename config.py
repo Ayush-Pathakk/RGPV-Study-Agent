99b48dd60f94f7983db23554394b688188f70c65
Ayush Pathak <ayush.teva@gmail.com>
2026-06-28 13:06:21 +0530

    madde changes in configuration for high level usage.

--- config.py
+++ config.py
@@ -8,14 +8,18 @@ PDF_DIR = "data/pdfs"
 CHROMA_DIR = "data/chroma_db"
 COLLECTION_NAME = "rgpv_notes"
 
-EMBED_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+EMBED_MODEL_NAME = "BAAI/bge-large-en-v1.5"
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_MODEL_NAME = "llama-3.3-70b-versatile"
 
 CHUNK_SIZE = 512
-CHUNK_OVERLAP = 50
-MAX_NEW_TOKEN = 400
+CHUNK_OVERLAP = 128
+RETRIEVAL_TOP_K = 10
+RERANKER_TOP_N  = 3
+RERANKER_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"
+
+MAX_NEW_TOKEN = 2048
 MIN_NEW_TOKEN = 1
 TOP_P = 1
 TOP_K = 3
